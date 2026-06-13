5c978572852e1aa227115d1690b659767968f13b
Erik Dickens <erik.dickens@gmail.com>
2026-06-13 13:29:08 +0200

    test

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,9 +4,6 @@ import { DATABASE_URL } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	// db.select().from('players').then((players) => {
-	// 	console.log(players);
-	// });
 	console.log('database url: ', DATABASE_URL);
 	const players = await db.select().from(player);
 	console.log(players);
